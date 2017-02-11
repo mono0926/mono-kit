cb0c13831dff12fa47444a896d022559a023fd10
Masayuki Ono <mono0926@gmail.com>
2017-02-11 16:13:34 +0900

    Install libraries

---
+++ Lib/Lib.h
@@ -0,0 +1,19 @@
+//
+//  Lib.h
+//  Lib
+//
+//  Created by mono on 2017/02/11.
+//  Copyright © 2017 mono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lib.
+FOUNDATION_EXPORT double LibVersionNumber;
+
+//! Project version string for Lib.
+FOUNDATION_EXPORT const unsigned char LibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lib/PublicHeader.h>
+
+
---
+++ Pinboard/Pinboard.h
@@ -0,0 +1,19 @@
+//
+//  Pinboard.h
+//  Pinboard
+//
+//  Created by mono on 2017/02/11.
+//  Copyright © 2017 mono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pinboard.
+FOUNDATION_EXPORT double PinboardVersionNumber;
+
+//! Project version string for Pinboard.
+FOUNDATION_EXPORT const unsigned char PinboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pinboard/PublicHeader.h>
+
+
